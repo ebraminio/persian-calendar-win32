61f5a5f057797815823d5949131519789019661e
iFarbod <ifarbod@users.noreply.github.com>
2025-10-30 22:15:44 +0330

    Fix linker error on MinGW

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ i686-w64-mingw32-gcc main.c -o persian-calendar.exe \
   -Wl,--gc-sections -Wl,--strip-all \
   -nostartfiles -fno-unwind-tables -fno-asynchronous-unwind-tables \
   -fno-stack-protector -fno-ident -fno-builtin \
-  -lkernel32 -luser32 -lshell32 -lgdi32 -lShlwapi \
+  -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi \
   -Wl,-e,_WinMainCRTStartup \
   -Wl,-subsystem,windows \
   -fmerge-all-constants -fno-plt # && wine persian-calendar.exe
