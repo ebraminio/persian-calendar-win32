09bfcbc0b111ffd1717f34042c7177abdb00af8e
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-07 17:46:14 +0330

    Fix msvc build

---
+++ build-msvc.bat
@@ -0,0 +1,1 @@
+cl main.cc /Fepersian-calendar.exe /utf-8 /MT /Os /O1 /GL /Gy /GS- /EHs-c- /GR- /volatile:iso /Zl /GF- /Gm- /fp:except- /d2finline- /link /NODEFAULTLIB /ENTRY:"WinMainCRTStartup" /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /INCREMENTAL:NO /LTCG:OFF kernel32.lib user32.lib shell32.lib gdi32.lib shlwapi.lib advapi32.lib
