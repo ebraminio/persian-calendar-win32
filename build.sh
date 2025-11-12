0714717f7efcefa90243ed83138fc1b622d359ff
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-13 01:24:11 +0330

    Cleanup build flags

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 i686-w64-mingw32-gcc main.cc -o persian-calendar.exe \
-    -fno-rtti -fno-exceptions -Oz -s -ffunction-sections \
-    -fdata-sections -Wl,--gc-sections -Wl,--strip-all -nostartfiles \
-    -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-stack-protector \
-    -fno-ident -fno-builtin -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 \
-    -Wl,-e,_WinMainCRTStartup -Wl,-subsystem,windows \
-    -nostdlib -nodefaultlibs -fmerge-all-constants -fno-plt
+    -fno-rtti -fno-exceptions -Oz -s \
+    -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 \
+    -nostartfiles -Wl,-e,_WinMainCRTStartup
