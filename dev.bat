9c435cb733633d2f348174ec730af821a9df8013
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-15 18:31:42 +0330

    Run the test in dev.bat

--- dev.bat
+++ dev.bat
@@ -1,4 +1,5 @@
 REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x8
-@taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" && .\build.bat && dir persian-calendar.exe && persian-calendar.exe
-cl test.cc && test.exe
+@taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" ^
+    && cl test.cc && test.exe ^
+    && .\build.bat && dir persian-calendar.exe && persian-calendar.exe
 REM dumpbin /DISASM persian-calendar.exe
