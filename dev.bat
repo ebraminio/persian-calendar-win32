01faa7dd7e8246258fd4e2825af4e5f2a4b4a9ae
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-16 13:53:20 +0330

    Minor

--- dev.bat
+++ dev.bat
@@ -1,4 +1,4 @@
-REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x8
+REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 @taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" ^
     && cl test.cc && test.exe ^
     && .\build.bat && dir persian-calendar.exe && persian-calendar.exe
