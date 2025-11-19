7b463572b2bdf590f99dd3babc4ac85deed4afe3
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-19 11:13:16 +0330

    Switch the llvm builder to the official llvm installation
    This one has apparently has less bugs and that global variables
    link crash doesn't happen here.
    
    Also contrary to what is claimed in the previous change, turned out
    SecureZeroMemory is an header only function so it can be used in the older
    versions of Windows also in theory.

--- build.llvm.bat
+++ build.llvm.bat
@@ -1,9 +1,10 @@
 @echo off
-REM https://github.com/mstorsjo/llvm-mingw
-C:\llvm-mingw\bin\i686-w64-mingw32-gcc persian-calendar.cc -o persian-calendar.llvm.exe ^
-    -Weverything -Wall -Wextra -Wpedantic -Werror -Wno-c++98-compat-pedantic ^
-    -fno-exceptions -fno-rtti -Oz -s -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 ^
-    -nostdlib -nodefaultlibs -nostartfiles -Wl,-e,start -Wl,-subsystem,windows ^
+REM https://releases.llvm.org/download.html Install from the Windows installer, e.g. LLVM-18.1.8-win32.exe
+"C:\Program Files (x86)\LLVM\bin\clang" persian-calendar.cc -o persian-calendar.llvm.exe ^
+    -Weverything -Wall -Wextra -Wpedantic -Werror ^
+    -Wno-c++98-compat-pedantic -Wno-unsafe-buffer-usage -Wno-nonportable-system-include-path ^
+    -fno-exceptions -fno-rtti -Oz -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 ^
+    -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld-link ^
     -Wl,/entry:start -Wl,/subsystem:windows ^
     -Wl,/merge:.rdata=.text -Wl,/merge:.data=.text ^
     -Wl,/fixed
