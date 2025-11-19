9cd750a713c81beb57dcd29c5874010f1796ffba
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-19 23:29:47 +0330

    Use clang cross platform capabilities to build in macOS

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-i686-w64-mingw32-gcc persian-calendar.cc -o persian-calendar.exe \
+clang --target=i686-w64-mingw32 --sysroot=$(brew --prefix mingw-w64)/toolchain-i686 \
+    persian-calendar.cc -o persian-calendar.exe \
     -Wall -Wextra -Wpedantic -Werror -Weffc++ -fno-exceptions -Oz -s \
     -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 \
-    -nostdlib -nodefaultlibs -nostartfiles -Wl,-e,start -Wl,-subsystem,windows \
+    -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld -Wl,-e,start -Wl,-subsystem,windows \
     && wine persian-calendar.exe
