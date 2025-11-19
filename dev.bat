182b2db93aa104f211ffd2cd661b7952e7297e34
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-19 20:36:43 +0330

    Preparation to move away from msvc as the main compiler

--- dev.bat
+++ dev.bat
@@ -2,5 +2,5 @@
 REM call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 taskkill /IM persian-calendar.exe /FI "STATUS eq RUNNING" ^
     && cl test.cc && test.exe ^
-    && build.bat && build.llvm.bat && dir persian-calendar*.exe && persian-calendar.exe && build.gcc.bat
+    && build.msvc.bat && build.llvm.bat && dir persian-calendar*.exe && persian-calendar.exe && build.gcc.bat
 REM dumpbin /DISASM persian-calendar.exe
