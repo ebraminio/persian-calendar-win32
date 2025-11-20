7d8611aea4f432213c5425a688fc62754acbcf60
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-20 09:44:01 +0330

    ci: Try to fix postlink

--- postlink.py
+++ postlink.py
@@ -2,8 +2,9 @@
 import pefile
 from pathlib import Path
 
-exe_path = str(Path("persian-calendar.exe").resolve())
-pe = pefile.PE(exe_path)
+with open('persian-calendar.exe', 'rb') as f:
+    pe_data = f.read()
+pe = pefile.PE(data=pe_data)
 pe.FILE_HEADER.TimeDateStamp = 0
 opt = pe.OPTIONAL_HEADER
 # 4.0 is Windows 4, the actual support is from Windows XP
@@ -11,4 +12,4 @@ opt.MajorOperatingSystemVersion = 4
 opt.MinorOperatingSystemVersion = 0
 opt.MajorSubsystemVersion = 4
 opt.MinorSubsystemVersion = 0
-pe.write(exe_path)
+pe.write('persian-calendar.exe')
