826e093b620e5260ea64c660893b86765bc232c9
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-20 11:28:35 +0330

    Sync build.bat and build.sh

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 REM https://releases.llvm.org/download.html Install from the Windows installer, e.g. LLVM-18.1.8-win32.exe
 "C:\Program Files (x86)\LLVM\bin\clang" persian-calendar.cc -o persian-calendar.exe ^
-    -Weverything -Wall -Wextra -Wpedantic -Werror ^
+    -Weverything -Wall -Wextra -Wpedantic -Werror -Weffc++ ^
     -Wno-c++98-compat-pedantic -Wno-nonportable-system-include-path ^
     -fno-exceptions -fno-rtti -Oz -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 ^
     -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld-link ^
