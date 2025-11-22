8cd046c65c3cc99fc3f2c4e7ed2fe5eba77aab6c
Ebrahim Byagowi <ebrahim@gnu.org>
2025-11-22 18:32:42 +0330

    Remove .data to .text section merge request
    No .data is created by default now

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ REM https://releases.llvm.org/download.html Install from the Windows installer, 
     -fno-exceptions -fno-rtti -Oz -lkernel32 -luser32 -lshell32 -lgdi32 -lshlwapi -ladvapi32 ^
     -nostdlib -nodefaultlibs -nostartfiles -fuse-ld=lld-link ^
     -Wl,/entry:start -Wl,/subsystem:windows ^
-    -Wl,/merge:.rdata=.text -Wl,/merge:.data=.text -Wl,/fixed
+    -Wl,/fixed -Wl,/merge:.rdata=.text
--- build.msvc.bat
+++ build.msvc.bat
@@ -4,4 +4,4 @@ cl persian-calendar.cc /Fepersian-calendar.msvc.exe ^
     /utf-8 /O1 /Wall /GS- /wd4710 /wd4711 /wd5045 /WX ^
     /link /ENTRY:start /NODEFAULTLIB /SUBSYSTEM:WINDOWS,5.01 /INCREMENTAL:NO ^
     kernel32.lib user32.lib shell32.lib gdi32.lib shlwapi.lib advapi32.lib ^
-    /FIXED /MERGE:.rdata=.text /MERGE:.data=.text /ignore:4254
+    /FIXED /MERGE:.rdata=.text /ignore:4254
